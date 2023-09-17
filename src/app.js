2784dbf7e479be351c6c908e5215ff3bba712764
Jose Luis Condori Chambi <informaticajlcc@gmail.com>
2023-09-17 18:54:50 -0400

    Se agregaron dependencias nuevas

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import express from 'express'
 //El nombre de persona puede ser cualquiera
 import personaRoutes from './routes/persona.routes.js'
-
+import usuarioRoutes from './routes/usuario.routes.js'
 
 const app = express();
 
 app.use(express.json());
 
 app.use(personaRoutes);
+app.use(usuarioRoutes)
+
 
 app.use((req, res) => {
     res.status(404).json({
